ff9e33c61f1a898e4c339ae03a07cbdbd8bbc3ba
Maycon <mayconn.aguiarr@gmail.com>
2018-06-03 18:04:05 -0300

    adicionado footer.html e modificado index.css, index.html e index.js

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ jQuery(document).ready(function(){
 	$(function(){
 		$("#content-header").load("header.html"); 
 		$("#content-body").load("content.html"); 
+		$("#content-footer").load("footer.html"); 
 	});
 }); 	
 
